2c4bf955613d9df87a388217a61fd9bec5659367
Zack Seliger <zseliger@gmail.com>
2020-07-02 13:48:04 -0500

    creating database query

--- database.py
+++ database.py
@@ -8,8 +8,10 @@ print(getenv("DB_PASSWORD"))
 db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    passwd= getenv("DB_PASSWORD"),
-    database = "biglistgames"
+    passwd = getenv("DB_PASSWORD")
 )
 
 mycursor = db.cursor()
+
+# create database and tables if they don't already exist
+mycursor.execute("CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"))
