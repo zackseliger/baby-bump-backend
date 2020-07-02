2980cc96971ed481f7f7709e4f255262c4469e3c
Zack Seliger <zseliger@gmail.com>
2020-07-02 13:13:34 -0500

    added .env

--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 import mysql.connector
+from os import getenv
+from dotenv import load_dotenv
+load_dotenv()
+
+print(getenv("DB_PASSWORD"))
 
 db = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    passwd= "babybump",
-    database = "testdatabase"
-    )
+    passwd= getenv("DB_PASSWORD"),
+    database = "biglistgames"
+)
 
 mycursor = db.cursor()
