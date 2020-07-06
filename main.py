59325a926e10747227c41105c4ac1b905e4c3080
mady-shank <67336447+mady-shank@users.noreply.github.com>
2020-07-05 22:02:25 -0400

    Create User Table
    setting up User Table

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+from forms import RegistrationForm, LoginForm
+import mysql.connector
+
 app = Flask(__name__)
+app.config['SECRET KEY'] = '009d6ca23ae690fec6bada88ac46f0b4'
 
 @app.route('/')
 def root():
 	return "<h1>Hello World!</h1>"
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+#@app.route('/register', method = ['POST'])
+#def register():
+#    Username = request.args.get('username');
+#    Password = request.args.get('password');
+#    Email = request.args.get('email');
+#    Role = request.args.get('role');
+#    insert_query = "INSERT INTO User (username, password, email, role) VALUES (%s, %s, %s, %s)"
+#    insert_values = (Username, Password, Email, Role);
+
+    mycursor.execute(insert_query, insert_values);
+    db.commit()
 
 
 # start the flask app
