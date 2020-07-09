313257359dd4d2c36e0a3ec8efe96694ade3a8d5
mady-shank <67336447+mady-shank@users.noreply.github.com>
2020-07-09 19:52:02 -0400

    Adding Routes
    organizing code, adding routes

---
+++ database.py
@@ -0,0 +1,22 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+
+
+#calling the .env file
+load_dotenv()
+password = os.getenv("password_db")
+
+#connect to server
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = password,
+    database = "babybump"
+)
+
+mycursor = db.cursor()
+
+# create database and tables if they don't already exist
+mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
+mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
--- main.py
+++ main.py
@@ -1,46 +1,58 @@
-from flask import Flask, request
+from flask import Flask, request, session, redirect, url_for, flash
+from flask_login import logout_user
 import mysql.connector
 import os
 from os import getenv
-from dotenv import load_dotenv, find_dotenv
-#print(getenv("DB_PASSWORD"))
+from dotenv import load_dotenv
+import database
 
 app = Flask(__name__)
 
+#Set the secret key
+load_dotenv()
+app.config["SECRET_KEY"] = os.getenv("secret_key")
+app.config['SESSION_TYPE'] = 'filesystem'
+
+#main route
 @app.route('/')
 def root():
-    return "Hello World"
+    if 'username' in session:
+        return "Logged in as %s" % escape(session['username'])
+    return "You are not logged in"
+
+#register route
+@app.route('/register')
+def register():
+   #request User registration info
+    Username = request.args.get("username")
+    Password = request.args.get("password")
+    Email = request.args.get("email")
+    Role = request.args.get("role")
+
+    #add user info to the table User
+    add_user = "INSERT INTO User (username, password, email, role) VALUES (%s, %s, %s, %s)"
+    user_info = (Username, Password, Email, Role)
+    database.mycursor.execute(add_user, user_info)
+    database.db.commit()
+    database.db.close()
+
+    return "Registration Complete"
+
+#login route
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('root'))
+    return 'You are logged in'
+
+#logout route
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('root'))
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
 
-#calling the load_dotenv file
-load_dotenv(find_dotenv())
-password = os.getenv("password_db")
-
-#connect to server
-db = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "babybump",
-    database = "babybump"
-)
-
-mycursor = db.cursor()
-
-# create database and tables if they don't already exist
-mycursor.execute("CREATE DATABASE IF NOT EXISTS babybump DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;".format("babybump"));
-mycursor.execute("CREATE TABLE IF NOT EXISTS User (username VARCHAR(25), password VARCHAR(25), email VARCHAR(25), role VARCHAR(25), id int PRIMARY KEY AUTO_INCREMENT)")
-
-#add user info to the table User
-Username = request.args.get("username")
-Password = request.args.get("password")
-Email = request.args.get("email")
-Role = request.args.get("role")
-
-add_user = "INSERT INTO User (username, password, email, role) VALUES (%s, %s, %s, %s)"
-user_info = (Username, Password, Email, Role)
-mycursor.execute(add_user, user_info)
-db.commit()
-
-db.close()
